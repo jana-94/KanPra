fe7c5891de5324f77caaa61034e445e647e946e5
jana-94 <janaagilesoftlabs@gmail.com>
2025-05-16 10:41:29 +0530

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    './app/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        comfortaa: ['var(--font-comfortaa)', 'cursive', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
